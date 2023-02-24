f9cd9df7640473b37fe0d6397a1f0c6bf35b2a58
Gustavo Favero <gustavo.vfavero@gmail.com>
2023-02-24 00:35:14 -0300

    Trabalhando com imagens utilizando Screen Readers do Tailwind para acessibilidade a leitores de tela.

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -7,6 +7,8 @@ const Article = ({title, text, tags, image, alt}) => {
           text.map((content, index) => <p key={index} className="text-alura-200 dark:text-gray-400">{content}</p>)
         }
       </div>
+      { image && <img className="sm:p-4" src={image} /> }
+      {image && alt && <span className="sr-only">{alt}</span>}
     </div>
   )
 }
