1563d2b1fb9513fb76c295f4f38f2c7ca624f442
Gustavo Favero <gustavo.vfavero@gmail.com>
2023-02-27 23:33:03 -0300

    Componetizando os inputs para evitar repetição de códigos desnecessariamente e possibilidade de futuras mudanças.

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,10 @@
+const Input = (props) => {
+  return (
+    <input
+      className="py-1 pl-5 w-full max-w-lg dark:bg-dark-200 text-dark-200 dark:text-gray-200 rounded-full outline-none dark:outline-none"
+      {...props}
+    />
+  );
+};
+
+export default Input;
