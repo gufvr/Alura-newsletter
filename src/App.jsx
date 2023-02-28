9d56db44bad249f1bbfdfa41fc81afd4a5920e65
Gustavo Favero <gustavo.vfavero@gmail.com>
2023-02-27 22:09:31 -0300

    Iniciando a construção de um formulário utilizando useState para definição de nomes ao serem inseridos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
+
 import ArticleList from "./components/ArticleList/ArticleList";
+import Form from "./components/Form/Form";
 import Header from "./components/Header/Header";
 
 const App = () => {
+  const [user, setUser] = useState();
+  const hasUser = Boolean(user);
   return (
     <>
-      <Header />
-      <ArticleList />
+      <Header user={user} />
+      {hasUser && <ArticleList />}
+      {hasUser || <Form onSubmit={setUser} />}
     </>
   );
 };
