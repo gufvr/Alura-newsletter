ca0b8062551ea58c9da4a449836bdd0a4796796e
Gustavo Favero <gustavo.vfavero@gmail.com>
2023-02-28 22:39:33 -0300

    Entendendo mais sobre as camadas base, components, utilities e layers do Tailwind e sobre encapsulamento.

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,3 @@
-import Input from "../Input/Input";
-
 const Form = ({ onSubmit }) => {
   const safeSubmit = (event) => {
     event.preventDefault();
@@ -14,8 +12,8 @@ const Form = ({ onSubmit }) => {
       onSubmit={safeSubmit}
       className="h-full flex flex-col items-center justify-center gap-10 -mt-20 mx-5"
     >
-      <Input type="text" required placeholder="Insira seu nome..." />
-      <Input type="email" required placeholder="Insira seu e-mail..." />
+      <input type="text" className="alura-input" required placeholder="Insira seu nome..." />
+      <input type="email" className="alura-input" required placeholder="Insira seu e-mail..." />
       <button
         type="submit"
         className="py-1 px-5 w-full max-w-sm bg-alura-100 dark:bg-dark-200 rounded-full text-gray-200 dark:text-gray-200
